fb7ae7210080d201040cfb91b1b3e4c76f9ba050
zaheer <zaheermalik284@gmail.com>
2019-02-15 10:30:52 +0500

    tweeks

--- src/main/java/com/example/cassandra/controller/UserController.java
+++ src/main/java/com/example/cassandra/controller/UserController.java
@@ -1,15 +1,14 @@
 package com.example.cassandra.controller;
 
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.cassandra.modal.User;
 import com.example.cassandra.repository.UserRepository;
+import com.google.common.base.Optional;
 
 @Controller
 public class UserController {
@@ -28,11 +27,29 @@ public class UserController {
 		User user=new User();
 		
 		user.setId(6);
-		user.setName("Zain");
+		user.setName("Abdullah");
 		user.setAddress("Sialkot");
 		user.setAge(18);
 		repository.save(user);
 		return "New User  Succeffully";
+	}
+	
+	@RequestMapping ("/update")
+	@ResponseBody
+	public Iterable<User> update() {
+		User user=new User();	
+		user.setId(5);
+		user.setName("Zain-ul-Abideen");
+		repository.save(user);
+		Iterable<User> users=repository.findAll();
+		return users;
+	}
+	
+	@RequestMapping ("/delete")
+	@ResponseBody
+	public String delete() {
+		repository.deleteById(4);
+		return "Deleted";
 	}
 	
 	@RequestMapping ("/allusers")
